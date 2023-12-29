09c2134bfa5b9818aa32b3e790f9ea91dd094f9c
Jazzmedo <mohamedmelawany@hotmail.com>
2023-12-29 11:02:21 +0200

    1.0

---
+++ js/script.js
@@ -0,0 +1,101 @@
+let inputs = document.querySelectorAll('.inum');
+
+inputs.forEach(input => {
+    input.addEventListener('input', function () {
+        document.document.querySelectorAll('.inum').style.backgroundColor =
+            event.target.value === '0' ? '#23193D' : '#50A501';
+        });
+    });
+function ttaaaal5() {
+    event.preventDefault();
+    var inpu = document.querySelectorAll('.c1');
+    var sum = 0;
+    inpu.forEach(input => {
+        sum += Number(input.value);
+    });
+    document.querySelector('.c1f').value = sum;
+    //asdas
+    var inpu = document.querySelectorAll('.c2');
+    var sum = 0;
+    inpu.forEach(input => {
+        sum += Number(input.value);
+    });
+    document.querySelector('.c2f').value = sum;
+    //asdas
+    var inpu = document.querySelectorAll('.c3');
+    var sum = 0;
+    inpu.forEach(input => {
+        sum += Number(input.value);
+    });
+    document.querySelector('.c3f').value = sum;
+    //asdas
+    var inpu = document.querySelectorAll('.c4');
+    var sum = 0;
+    inpu.forEach(input => {
+        sum += Number(input.value);
+    });
+    document.querySelector('.c4f').value = sum;
+    //asdas
+    var inpu = document.querySelectorAll('.c5');
+    var sum = 0;
+    inpu.forEach(input => {
+        sum += Number(input.value);
+    });
+    document.querySelector('.c5f').value = sum;
+    //asdas
+    var inpu = document.querySelectorAll('.c6');
+    var sum = 0;
+    inpu.forEach(input => {
+        sum += Number(input.value);
+    });
+    document.querySelector('.c6f').value = sum;
+    //asdas
+    var inpu = document.querySelectorAll('.c7');
+    var sum = 0;
+    inpu.forEach(input => {
+        sum += Number(input.value);
+    });
+    document.querySelector('.c7f').value = sum;
+    //asdas
+    var inpu = document.querySelectorAll('.c8');
+    var sum = 0;
+    inpu.forEach(input => {
+        sum += Number(input.value);
+    });
+    document.querySelector('.c8f').value = sum;
+}
+function doubleValues1() {
+    var myInputs = document.querySelectorAll(".r1");
+    event.preventDefault();
+    myInputs.forEach(function (input) {
+        input.value = parseInt(input.value) * 2;
+    });
+}
+function doubleValues2() {
+    var myInputs = document.querySelectorAll(".r2");
+    event.preventDefault();
+    myInputs.forEach(function (input) {
+        input.value = parseInt(input.value) * 2;
+    });
+}
+function doubleValues3() {
+    var myInputs = document.querySelectorAll(".r3");
+    event.preventDefault();
+    myInputs.forEach(function (input) {
+        input.value = parseInt(input.value) * 2;
+    });
+}
+function doubleValues4() {
+    var myInputs = document.querySelectorAll(".r4");
+    event.preventDefault();
+    myInputs.forEach(function (input) {
+        input.value = parseInt(input.value) * 2;
+    });
+}
+function doubleValues5() {
+    var myInputs = document.querySelectorAll(".r5");
+    event.preventDefault();
+    myInputs.forEach(function (input) {
+        input.value = parseInt(input.value) * 2;
+    });
+}
